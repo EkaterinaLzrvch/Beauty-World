c2def3551be6ad814c3f5b878e17a71004335373
Ekaterina Lazarevich <lazarevich.1012@mail.ru>
2022-10-09 23:09:19 +0700

    sprint2

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const { src, dest, series, watch, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const del = require('del');
+
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+// const clean = require('gulp-clean');
+
+// Таск компиляции SASS в CSS
+function buildSassDev() {
+    return src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+        .pipe(
+            postcss([
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ['last 2 versions']
+                }),
+                cssnano()
+            ])
+        )
+        .pipe(sourcemaps.write())
+        .pipe(dest('src/css'))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream());
+}
+
+function buildSass() {
+    return src('src/scss/**/*.scss')
+        .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+        .pipe(
+            postcss([
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ['last 2 versions']
+                }),
+                cssnano()
+            ])
+        )
+        .pipe(dest('dist/css'));
+}
+
+// Таск работы с html файлами
+function buildHtml() {
+    return src('src/**/*.html')
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream());
+}
+
+// Таск копирования статичных файлов
+function copy() {
+    return src(['src/images/**/*.*'], { base: 'src' }).pipe(dest('dist'));
+}
+
+// Таск очистки dist
+function cleanDist() {
+    return del('dist');
+}
+
+// Таск отслеживания изменения файлов
+function serve() {
+    watch('src/scss/**/*.scss', buildSassDev);
+    watch('src/**/*.html', buildHtml);
+}
+
+// Создание дев-сервера
+function createDevServer() {
+    browserSync.init({
+        server: 'src',
+        notify: false
+    })
+}
+
+exports.sass = buildSass;
+exports.html = buildHtml;
+exports.copy = copy;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, buildSass, buildHtml, copy);
+exports.default = series(buildSassDev, parallel(createDevServer, serve));
+
