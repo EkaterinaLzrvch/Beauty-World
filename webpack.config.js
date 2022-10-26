497af28aa1b8b81db3b5d47ec103efadaa0925c9
Ekaterina Lazarevich <lazarevich.1012@mail.ru>
2022-10-26 22:01:40 +0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  devtool: isProd ? false : 'source-map',
+  devtool:'hidden-source-map',
   module: {
     rules: [
       {
