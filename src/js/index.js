4b6306e9bfb5af5f49186613d43bf3a7bfb79fd3
Ekaterina Lazarevich <lazarevich.1012@mail.ru>
2022-10-28 00:27:30 +0700

    27/10

--- src/js/index.js
+++ src/js/index.js
@@ -46,10 +46,73 @@ carousel();
 // данные из формы 
 document.querySelector('.form__btn').addEventListener('click', submitForm);
 
-function submitForm(e) {
+// function submitForm(e) {
+//   e.preventDefault();
+
+//   let clientName = document.querySelector('.client-name').value;
+//   let clientTel = document.querySelector('.client-phone').value;
+//   console.log(clientName + '\n' + clientTel);
+// };
+
+// function submitForm(e) {  //Заменим на массив:
+//   e.preventDefault();
+
+//   const nodes = document.forms["form"].querySelectorAll("input");
+//   const clientData = [].map.call(nodes, function(item) {
+//     return {name : item.name, value : item.value};
+//   });
+//   console.log(clientData);
+// }
+
+// Отправка данных из формы на сервер 
+
+const API_PATH = 'https://beauty-saloon-server.herokuapp.com/api';
+
+function submitForm(e) {  
   e.preventDefault();
 
-  let clientName = document.querySelector('.client-name').value;
-  let clientTel = document.querySelector('.client-phone').value;
-  console.log(clientName + '\n' + clientTel);
-};
+  const nodes = document.forms["form"].querySelectorAll("input");
+  const clientData = [].map.call(nodes, function (item) {
+    return { name: item.name, value: item.value };
+  });
+  console.log(clientData);
+
+  // fetch(`${API_PATH}/orders`, {
+  //   method: 'POST',
+  //   body: JSON.stringify(clientData)
+  // })
+
+  //   .then(() => {
+  //     console.log('Successful');
+  //   });
+
+  fetch(`${API_PATH}/login`, {
+    method: 'POST',
+    body: JSON.stringify(
+      {
+        userName: 'admin',
+        password: 'admin'
+      }
+    ),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+
+    .then(response => response.json())
+    .then(async ({ access_token }) => {
+      const response = await fetch(`${API_PATH}/orders`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        }
+      });
+      const orders = await response.json();
+      console.log(orders);
+
+    })
+
+
+}
+
+//надо отправить clientData
