6e3f8f575c97e5485caaebe0db97753fa0de68cd
Ekaterina Lazarevich <98206801+EkaterinaLzrvch@users.noreply.github.com>
2022-11-02 22:30:51 +0700

    Update webpack.config.js

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  devtool: 'inline-source-map',
+  devtool: isProduction ? false : 'eval-source-map',
   module: {
     rules: [
       {
